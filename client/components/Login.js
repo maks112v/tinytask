0322a323702d01b2ab3a6883f41867390f27b8c3
Maksim V <maks112v@gmail.com>
2020-07-29 15:42:44 -0400

    Update Login.js

--- client/components/Login.js
+++ client/components/Login.js
@@ -6,7 +6,7 @@ import Button from "./Button";
 export default function LoginPage() {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <Seo titles={["Login", "My.Church"]} />
+      <Seo titles={["Login", "Tasks"]} />
       <div className="text-center w-full max-w-xs p-2">
         <h1 className="text-4xl md:text-5xl">Sign In</h1>
         <p>Let's get you up and running...</p>
